9c38c07fad643781caeb9ac033a0058147a92817
Jean Carlos Antonio Pereira <jean-bra@protonmail.com>
2022-08-30 09:59:36 -0300

    Finalização do tabuleiro.

---
+++ tabuleiro/components/Subdivisao.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Subdivisao.module.css";
+
+export default function Subdivisao(props) {
+    return (
+        <div 
+        style={{backgroundColor: props.preta ? "black" : "white"}}
+        className={styles.subdivisao}></div>
+    );
+}
